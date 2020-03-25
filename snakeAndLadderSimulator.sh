589d09cd0db9341e49b66aa941e4e08aaf8ce58f
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-25 16:04:27 +0530

    Refactored: Player Checking For The Options

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -x
 
+#CONSTANTS
+IS_NO_PLAY=0
+IS_LADDER=1
+IS_SNAKE=2
+
 #INITIAL PLAYER POSITION
 playerPosition=0
 
@@ -9,5 +14,33 @@ function rollDice(){
 	echo $dice
 }
 
+#FUNCTION FOR GENERATING OPTIONS
+function generateOption(){
+	option=$(( RANDOM%3 ))
+	echo $option
+}
+
 #CALLING THE FUNCTION
 dice=$( rollDice )
+option=$( generateOption )
+
+#PLAYER CHECKING FOR THE OPTION
+case $option in
+	$IS_NO_PLAY)
+		playerPosition=0
+	;;
+	$IS_LADDER)
+		playerPosition=$(( $playerPosition+$dice ))
+	;;
+	$IS_SNAKE)
+		if (( $(( $playerPosition-$dice ))>=0 ))
+		then
+			playerPosition=$(( $playerPosition-$dice ))
+		else
+			playerPosition=0
+		fi
+	;;
+	*)
+		printf "Invalid"
+	;;
+esac
