cf9cf3003d7bf298540a028835ebc15fc66d580b
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-25 15:46:50 +0530

    Refactored: Rolling The Dice

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -2,3 +2,12 @@
 
 #INITIAL PLAYER POSITION
 playerPosition=0
+
+#FUNCTION TO ROLL THE DICE 
+function rollDice(){
+	dice=$(( 1+RANDOM%6 ))
+	echo $dice
+}
+
+#CALLING THE FUNCTION
+dice=$( rollDice )
