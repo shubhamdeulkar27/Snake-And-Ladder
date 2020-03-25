4377f4dca633146da92eb5ff33ad45dcd0a53098
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-25 19:23:45 +0530

    Refactored: Repeating the Each Step Till PlayerPosition equals 100

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -4,6 +4,7 @@
 IS_NO_PLAY=0
 IS_LADDER=1
 IS_SNAKE=2
+IS_WIN=100
 
 #INITIAL PLAYER POSITION
 playerPosition=0
@@ -20,27 +21,31 @@ function generateOption(){
 	echo $option
 }
 
-#CALLING THE FUNCTION
-dice=$( rollDice )
-option=$( generateOption )
 
-#PLAYER CHECKING FOR THE OPTION
-case $option in
-	$IS_NO_PLAY)
-		playerPosition=0
-	;;
-	$IS_LADDER)
-		playerPosition=$(( $playerPosition+$dice ))
-	;;
-	$IS_SNAKE)
-		if (( $(( $playerPosition-$dice ))>=0 ))
-		then
-			playerPosition=$(( $playerPosition-$dice ))
-		else
-			playerPosition=0
-		fi
-	;;
-	*)
-		printf "Invalid"
-	;;
-esac
+while [ $playerPosition -ne $IS_WIN ]
+do
+	#CALLING THE FUNCTION
+	dice=$( rollDice )
+	option=$( generateOption )
+
+	#PLAYER CHECKING FOR THE OPTION
+	case $option in
+		$IS_NO_PLAY)
+			continue
+		;;
+		$IS_LADDER)
+			playerPosition=$(( $playerPosition+$dice ))
+		;;
+		$IS_SNAKE)
+			if (( $(( $playerPosition-$dice ))>=0 ))
+			then
+				playerPosition=$(( $playerPosition-$dice ))
+			else
+				playerPosition=0
+			fi
+		;;
+		*)
+			printf "Invalid"
+		;;
+	esac
+done
