21879f143c3753088ca57d5c78b3e5b6a0fc6436
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-26 10:55:16 +0530

    Refactored: Playing Game With Two Players

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -7,8 +7,11 @@ IS_SNAKE=2
 IS_WIN=100
 
 #VARIABLES
-playerPosition=0
-diceCount=0
+playerOne=0
+playerTwo=0
+diceOneCount=0
+diceTwoCount=0
+playerTurn=0
 
 #FUNCTION TO ROLL THE DICE 
 function rollDice(){
@@ -22,41 +25,82 @@ function generateOption(){
 	echo $option
 }
 
-declare -A playerPositions
-
-while [ $playerPosition -ne $IS_WIN ]
-do
-	#CALLING THE FUNCTION
+#FUNCTION FOR PLAY
+function play(){
+	local playerPosition=0
 	dice=$( rollDice )
 	option=$( generateOption )
-
-	#PLAYER CHECKING FOR THE OPTION
 	case $option in
 		$IS_NO_PLAY)
-			continue
+			playerPosition=0
 		;;
 		$IS_LADDER)
-			if (( $(( $playerPosition+$dice ))>$IS_WIN ))
-			then
-				continue
-			else
-				playerPosition=$(( $playerPosition+$dice ))
-			fi
+			playerPosition=$(( $playerPosition+$dice ))
 		;;
 		$IS_SNAKE)
-			if (( $(( $playerPosition-$dice ))>=0 ))
-			then
-				playerPosition=$(( $playerPosition-$dice ))
-			else
-				playerPosition=0
-			fi
+			playerPosition=$(( $playerPosition-$dice ))
 		;;
 		*)
 			printf "Invalid"
 		;;
 	esac
-	positions[ (( ++diceCount )) ]=$playerPosition
+echo $playerPosition
+}
+
+#DICTIONARIES TO STORE BOTH PLAYERS MOVES
+declare -A playerOneMoves
+declare -A playerTwoMoves
+
+#PALYING GAME WITH TWO PLAYERS
+while [ true ]
+do
+	playGame=$( play )
+	if (( $(( $playerTurn%2 ))==0 ))
+	then
+		if (( $playGame>0 ))
+		then
+			if (( $(( $playerOne+$playGame ))<=$IS_WIN ))
+			then
+				playerOne=$(( $playerOne+$playGame ))
+			else
+				continue
+			fi
+		elif (( $playGame<0 ))
+		then
+			if (( $(( $playerOne+$playGame ))>=0 ))
+			then
+				playerOne=$(( $playerOne+$playGame ))
+			else
+				continue
+			fi
+		fi
+	playerOneMoves[ (( diceOneCount++ )) ]=$playerOne
+	elif (( $(( $playerTurn%2 ))!=0 ))
+	then
+		if (( $playGame>0 ))
+		then
+			if (( $(( $playerTwo+$playGame ))<=$IS_WIN ))
+			then
+				playerTwo=$(( $playerTwo+$playGame ))
+			else
+				continue
+			fi
+		elif (( $playGame<0 ))
+		then
+			if (( $(( $playerTwo+$playGame ))>=0 ))
+			then
+				playerTwo=$(( $playerTwo+$playGame ))
+			else
+				continue
+			fi
+		fi
+		playerTwoMoves[ (( diceTwoCount++ )) ]=$playerTwo
+	fi
+	(( playerTurn++ ))
+
+	if (( $playerOne==$IS_WIN || $playerTwo==$IS_WIN ))
+	then
+		break
+	fi
 done
 
-echo ${!positions[@]}
-echo ${positions[@]}
